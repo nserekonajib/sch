efaec610873a164768706f3d4e344e510751464c
LUNSERK TECHNOLOGIES <109471084+nserekonajib@users.noreply.github.com>
2026-08-04 23:44:16 +0300

    Update REPORT_API_URL to production endpoint

--- routes/resultsCard/competenceReportCard.py
+++ routes/resultsCard/competenceReportCard.py
@@ -20,7 +20,7 @@ SUPABASE_KEY = os.getenv('SUPABASE_KEY')
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # Node.js API URL
-REPORT_API_URL = os.getenv('REPORT_API_URL', 'http://localhost:4000')
+REPORT_API_URL = os.getenv('REPORT_API_URL', 'http://d44cgg048cgckw4kwo4osw4k.195.200.15.127.sslip.io')
 
 competence_bp = Blueprint('competence', __name__, url_prefix='/competence-report')
 
